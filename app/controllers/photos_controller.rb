c2a6141ac798b2d0cec6446b865949d661b89a16
Cristina Costa <cncosta25@gmail.com>
2016-05-01 17:40:31 -0500

    forgot to do this to github :(

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,46 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+
+    #@photo_source = p.source
+    #@photo_caption = p.caption
+    #@photo_id = p.id
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    #render("create_row.html.erb")
+
+    redirect_to("/photos")
+  end
+
+
+  def destroy
+    delete = Photo.find_by({ :id => params[:id]})
+    delete.destroy
+
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    edit = Photo.find_by({ :id => params[:id]})
+    edit.caption = params[:the_caption]
+    edit.source = params[:the_source]
+    edit.save
+
+  end
+
+  def update_photo
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,28 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+# put more specific route above so it won't conflict with the /photos/:id below
+
+# Routes to CREATE photos
+
+  get("/photos/new", { :controller => "photos", :action => "new_form"})
+  get("/create_photo", { :controller => "photos", :action =>"create_row"})
+
   # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
+  get("/photos",          { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  # Delete Photo
+
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+  # Edit Photo
+
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+
+  # Update Row
+
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+
 
 end
