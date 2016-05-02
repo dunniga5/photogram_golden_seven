7715b6df9c929d70ada25c65f4e43aaa3df35cae
alathans <alathans@users.noreply.github.com>
2016-05-01 20:55:41 -0500

    9pm

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,13 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+  p = Photo.find_by({:id => params[:id]})
+  @photo_source = p.source
+  @photo_caption = p.caption
+  @photo_id = p.id
+
+    end
 
 end
