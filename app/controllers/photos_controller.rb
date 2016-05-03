2139ef7b6bbf9d0622f308c8483314cefff3bda3
Leland Brewster <jlbrew3@users.noreply.github.com>
2016-05-03 08:36:18 -0500

    create working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,16 @@ class PhotosController < ApplicationController
     @photo = Photo.find_by({ :id => params[:id]})
   end
 
+  def new_form
+    #code
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("/photos")
+  end
 end
