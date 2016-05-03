d12173f2f1a1786ba2ec48f5bcebd68e4568a400
alathans <alathans@users.noreply.github.com>
2016-05-02 20:54:18 -0500

    all functions in - but data not saving to database still
    even w/ updates.... hm.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,17 +9,39 @@ class PhotosController < ApplicationController
 
   def new_form
 
-
   end
 
   def create_row
 
-  p = Photo.new
-  p.caption = params[:caption]
-  p.source = params[:source]
-  p.save
+  @photo = Photo.new
+  @photo.caption = params[:caption]
+  @photo.source = params[:source]
+  @photo.save
 
   redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def destroy
+
+    photo = Photo.find_by({:id => params[:id]})
+    photo.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({:id => params[:id]})
+
+  end
+
+  def update_photo
+    @photo = Photo.find_by({:id => params[:id]})
+    @photo.caption = params[:caption]
+    @photo.source = params[:source]
+    @photo.save
+
+    redirect_to("http://localhost:3000/photos")
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get("/photos/new",       { :controller => "photos", :action => "new_form" })
   get("/create_photo",       { :controller => "photos", :action => "create_row" })
 
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/photos/:id/edit/update_photo", { :controller => "photos", :action => "update_photo" })
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
