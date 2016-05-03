7b34b9559bff8000e2444950786947c22d70f063
alathans <alathans@users.noreply.github.com>
2016-05-02 20:01:24 -0500

    can't get the new photo data to save to database

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,25 @@
 class PhotosController < ApplicationController
   def index
-    @list_of_photos = Photo.all
+  @list_of_photos = Photo.all
   end
 
   def show
   @photo = Photo.find_by({:id => params[:id]})
-    end
+  end
+
   def new_form
 
+
+  end
+
+  def create_row
+
+  p = Photo.new
+  p.caption = params[:caption]
+  p.source = params[:source]
+  p.save
+
+  redirect_to("http://localhost:3000/photos")
 
   end
 
