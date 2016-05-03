ee37b3bc1b8ad860759761383e683395f9f3628c
Mitch Emerson <mitchemerson@users.noreply.github.com>
2016-05-02 22:06:32 -0500

    need to fix update_row

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,41 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
 
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    # render("create_row.html.erb")
+    redirect_to("/photos")
+  end
+
+  def destroy
+    p = Photo.find_by({ :id => params[:id] })
+    p.destroy
+
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def update_row
+    p = Photo.find_by({ :id => params[:id] })
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("/photos/:id")
+  end
 end
