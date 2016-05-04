6a9fd74a8e3f82002d2932a529498fed50ce01cc
Starr Marcello <starr.marcello@users.noreply.github.com>
2016-05-03 23:46:42 -0500

    Stopping point 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,28 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id=> params[:id] })
+  end
 
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    # render("create_row.html.erb")
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    p = Photo.find(params[:id])
+    p.destroy = params[:id]
+
+    redirect_to("http://localhost:3000/photos")
+  end
 end
