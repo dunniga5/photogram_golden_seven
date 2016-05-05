2a5891cd807922e8f1b6b755c9920e7a02474a94
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-05-05 11:57:04 -0500

    Stopping_point_7
    Almost complete- just need to re-route edited photos to "show" page
    instead of to home page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -29,5 +29,15 @@ class PhotosController < ApplicationController
 
   def edit_form
     @photo_to_edit = Photo.find_by({ :id => params[:id] })
-end
+  end
+
+  def update_row
+    p = Photo.find_by({ :id => params[:id] })
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos/")
+
+  end
 end
