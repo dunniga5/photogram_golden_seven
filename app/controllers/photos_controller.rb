c9574557b9975257c9b249b71bab64eff50a7f46
Uday Gorrepati <uday.gorrepati@gmail.com>
2016-05-05 15:32:22 -0400

    Initial Photogram draft.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,39 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find params[:id]
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    @photo = Photo.new
+    @photo.source = params[:the_source]
+    @photo.caption = params[:the_caption]
+    @photo.save
+    redirect_to("/photos")
+  end
+
+  def destroy
+    photo = Photo.find params[:id]
+    photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find params[:id]
+  end
+
+  def update_row
+    photo = Photo.find params[:id]
+    photo.source = params[:the_source]
+    photo.caption = params[:the_caption]
+    photo.save
+    redirect_to("/photos/#{params[:id]}")
+  end
 
 end
