3f96093581ac02b03b244213bd7e44f5acb9e30b
tstrauts <tstrauts@chicagobooth.edu>
2016-05-05 00:52:22 -0500

    Tim Strauts HW Submission 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,38 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
 
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    x = Photo.find_by({ :id => params[:id]})
+    x.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
+
+  def update_row
+    y= Photo.find_by({ :id => params[:id]})
+    y.caption = params[:the_caption]
+    y.source = params[:the_source]
+    y.save
+    redirect_to("http://localhost:3000/photos/" + params[:id].to_s)
+  end
 end
