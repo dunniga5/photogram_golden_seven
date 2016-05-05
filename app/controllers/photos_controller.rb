33d0ea47b62b077177603f4cedf83404e3f3d153
Zachary C. Cornett <zcornett@chicagobooth.edu>
2016-05-05 00:00:42 -0500

    "Show" functionality added

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,42 @@ class PhotosController < ApplicationController
   end
 
 
+  def show
+    @photo = Photo.find_by ({ :id=> params[:id]})
+  end
+
+#DRY it up (Pre-DRYing)
+  # def show
+  #   p = Photo.find_by ({ :id=> params[:id]})
+  #   @photo_source = p.source
+  #   @photo_caption = p.caption
+  #   @photo_id = p.id
+  # end
+
+# Now the controller needs to know how to handle each variable
+# def show
+#   @photo.source
+#   @photo.caption
+#   @photo.id
+#   render ("show.html.erb")
+# end
+
+
+# This is the basic function of the "Show" action. After the hmtl portion has been created extract all of the variables and copy them here
+# def show
+#   render ("show.html.erb")
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
