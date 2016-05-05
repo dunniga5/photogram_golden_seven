9cb2aae64befcf7e176e2b11d45613d7a3ce9b45
Denis Bulanov <dbulanov@user.noreply.github.com>
2016-05-05 12:15:37 -0500

    all up to create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,15 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+    #@photo_source = p.source
+    #@photo_caption = p.caption
+    #@photo_id = p.id
+  end
+
+  def new_form
+      
+  end
 
 end
