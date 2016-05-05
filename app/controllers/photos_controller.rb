ed5ac78625d8cce1d94789062b1c6c3876f1786e
Chris Jacobson <cjacobson@stanfordalumni.org>
2016-05-05 10:58:49 -0500

    edited
    cannot get dynamic link to go to individual picture page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,35 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def new_photo
+
+  end
+
+  def create_photo
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("/photos")
+  end
+
+  def edit_page
+    @photo_edit = Photo.find_by({ :id => params[:id] })
+  end
+
+  def edit
+    i = Photo.find_by({ :id => params[:id] })
+    i.caption = params[:edit_caption]
+    i.source = params[:edit_source]
+    i.save
+
+    # cant get dynamic link to move
+    redirect_to("/photos/7")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
+  get("/photos/new", {:controller => "photos", :action => "new_photo"})
+  get("/create_photo",       { :controller => "photos", :action => "create_photo" })
+
+  get("/photos/:id/edit",       { :controller => "photos", :action => "edit_page" })
+  get("/edit/:id",       { :controller => "photos", :action => "edit" })
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+
 
 end
