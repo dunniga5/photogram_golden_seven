9af5f6b23cb5cf02dc1fd296b53814902c315426
shao313 <shaozhu@chicagobooth.edu>
2016-05-05 00:37:15 -0500

    final_without_fun

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,57 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
-def show
-  @id = params[:id]
-@url = Photo.find(@id).source
-@caption = Photo.find(@id).caption
+  def show
+    @id = params[:id]
+    @url = Photo.find(@id).source
+    @caption = Photo.find(@id).caption
 
-render("show.html.erb")
+    render("show.html.erb")
 
+  end
+
+  def new_form
+
+    render("new_form.html.erb")
+
+  end
+
+def create_row
+  @new_url=params[:new_url]
+  @new_caption=params[:new_caption]
+
+  x = Photo.new
+  x.source = @new_url
+  x.caption = @new_caption
+  x.save
+
+redirect_to("http://localhost:3000/photos")
+end
+
+def delete
+
+  @delete_id = params[:id]
+  x=Photo.find(@delete_id)
+  x.destroy
+
+redirect_to("http://localhost:3000/photos")
+end
+
+def edit_form
+@edit_id=params[:id]
+@form_source = Photo.find(@edit_id).source
+@form_caption = Photo.find(@edit_id).caption
+render("edit_form.html.erb")
+
+end
+
+def edit_row
+@change_id=params[:id]
+i = Photo.find(@change_id)
+i.source = params[:edit_url]
+i.caption=params[:edit_caption]
+i.save
+redirect_to("http://localhost:3000/photos")
 end
 
 end
