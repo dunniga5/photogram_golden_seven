833511e16f27cd1f0f4e010341ccf50a1cfaeb1b
Shana Dunnigan <dunniga5@users.noreply.github.com>
2016-05-06 18:02:52 -0500

    Photogram Commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,35 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo=Photo.find_by({ :id => params[:id]})
 
+  end
+  def new_form
+    #code
+  end
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("/photos")
+  end
+  def destroy
+    p = Photo.find(params[:id])
+    p.destroy
+    redirect_to("/photos")
+  end
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id]})
+
+  end
+  def update_row
+    p = Photo.find(params[:id])
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("/photos/"+params[:id])
+  end
 end
